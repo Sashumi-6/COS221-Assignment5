fc6265912f32608bf8e01dc6873de78595a42bb1
Ntuuu <mbatha05n@gmail.com>
2025-05-26 00:33:10 +0200

    added the deleteProduct endpoint

--- Config.php
+++ Config.php
@@ -323,6 +323,19 @@ private function getDescendantsRecursive($categories, $parentId) {
         
     }
 
+    public function deleteProduct($upc) {
+        
+        $query = "DELETE FROM products WHERE upc = ?";
+        $stmt = $this->prepare($query);
+        $stmt->bind_param('i', $upc);
+        
+        if (!$stmt->execute()) {
+            throw new Exception('Could not delete product from the database.');
+        }
+
+
+}
+
     /**
      * add a new product
      */
@@ -415,12 +428,14 @@ private function getDescendantsRecursive($categories, $parentId) {
         // Return true if any rows were affected
         return $stmt->affected_rows > 0;
     }
+
+    
     
     public function close() {
         $this->conn->close();
     }
     
-    // Add this method to access connection errors
+
     public function error() {
         return $this->conn->error;
     }
