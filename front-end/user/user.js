2456fd0ff043f4ed6e7781cd0689237ed4040206
Daniel Aidan Cohen <168676041+Sashumi-6@users.noreply.github.com>
2025-05-25 19:23:32 +0200

    restored user.js

--- front-end/user/user.js
+++ front-end/user/user.js
@@ -1,5 +1,208 @@
-function webLoad() {
+function productsUpdate(data) {
+    let del_btn = $(`<div class="buttons"><button>Delete</button></div>`);
+    let main = $(`
+        <div class="product" id="upc-${data.upc}">
+            <a>${data.upc}</a>
+            <a>${data.name}</a>
+            <a>${data.description}</a>
+            <a>${data.category}</a>
+            <a>${data.brand}</a>
+            <a>${data.supplier}</a>
+        </div>
+        `).append(del_btn);
+    $('#products-container').append(main);
+}
 
+function usersUpdate(data) {
+    let del_btn = $(`<div class="buttons"><button>Delete</button></div>`);
+    let main = $(`
+        <div class="user" id="userid-${data.id}">
+            <a>${data.id}</a>
+            <a>${data.username}</a>
+            <a>${data.fullName}</a>
+            <a>${data.email}</a>
+            <a>${data.user_type}</a>
+        </div>
+        `).append(del_btn);
+    $('#users-container').append(main);
+}
+
+function sideContentUpdate(selector, data) {
+    let appendableComponent = $("#" + selector + "-container" + " #list-items");
+    if ((selector == "categories" && CategoryInit == 0) || (selector == "stockist" && StockistInit == 0)) {
+        //if we havent loaded the page
+        let addbtn = $(`<button id="add-${selector}-btn">Add</button>`)
+            .click({id: "add-" + selector, type: selector, command: "add"}, sideContent_add_del);
+        
+        let addComponent = $(`
+            <div id="add-${selector}" class="item">
+                <input id="add-${selector}" type="text">
+                <div class="buttons"></div>
+            </div>
+            `);
+        addbtn.appendTo(addComponent.children('.buttons'));
+        appendableComponent.append(addComponent);
+        if (selector == "categories") CategoryInit = 1;
+        if (selector == "stockist") StockistInit = 1;
+    }
+    
+    // Each Category name is unique => using it as the unique identifier for components
+
+    //template
+
+    //Create the buttons dawg
+    let edit = $(`<button id="edit-btn">Edit</button>`)
+        .click({id: data, type: selector}, editSideContent);
+    let del = $(`<button id="delete-btn">Delete</button>`)
+        .click({id: data, type: selector, command: "del"}, sideContent_add_del);
+
+    let main = $(`
+        <div class="item" id="${data}">
+            <input id="inputEdit-${selector}${data}" class="hidden">
+            <a>${data}</a>
+            <div class="buttons"></div>
+        </div>
+        `)
+
+    edit.appendTo(main.children('.buttons'));
+    del.appendTo(main.children('.buttons'));
+    appendableComponent.append(main);
+}
+
+function editSideContent(event) {
+    let id = event.data.id;
+    let type = event.data.type;
+
+    let item = $('#' + type + "-container #" + id);
+    let del_btn = item.find('#delete-btn');
+    let upd_btn = item.find('#edit-btn');
+    let input = item.find("input");
+    let itemName = item.find("a");
+    if (upd_btn.text() == "Edit") {
+        upd_btn.text("Done");
+        del_btn.toggleClass('hidden');
+        itemName.toggleClass('hidden');
+        input.val(itemName.text());
+        input.toggleClass('hidden');
+    } else {
+        // API FUNCTIONALITY DONE HERE
+        // UPDATE EVERYWHERE WHERE THIS CATEGORY EXISTS
+        // THEN RELOAD ALL PRODUCTS ? - MAYBE CAN JUST TRY TO FIND WHERE THIS VAL EXITS IN THE PAGE
+        //      => IN THE CORRECT CONTEXT
+
+        upd_btn.text("Edit");
+        del_btn.toggleClass('hidden');
+        input.toggleClass('hidden');
+        itemName.text(input.val());
+        itemName.toggleClass('hidden');
+    }
+}
+
+function sideContent_add_del(event) {
+    let operation = event.data.command;
+    let type = event.data.type;
+    let id = event.data.id;
+
+    let addInputValue = $('#' + type + "-container #" + id + " input").val().trim();
+    if (operation == "add" && addInputValue !== '') {
+        let items = $('#' + type + "-container");
+        let item = items.find('#' + id);
+
+        if ($('.item#' + addInputValue).length == 0) {
+            sideContentUpdate(type, addInputValue);
+            item.find('input').attr('placeholder', 'Success')
+
+            // IMPLEMENT API SHIT HERE !!!
+        } else item.find('input').attr('placeholder', 'Error - Try again');
+        item.find('input').val('');
+    }
+
+    if (operation == "del") {
+        $('.item#' + id).remove();
+        // IMPLEMENT API SHIT HERE
+    }
+}
+
+// Tmp Data
+var categoryTmp = ["Category1", "Category2", "Category3", "Category4", "Category5", "Category6"]
+var StockistTmp = ["Stockist1", "Stockist2", "Stockist3", "Stockist4", "Stockist5", "Stockist6"]
+
+let products = [
+    {
+        upc: "0001",
+        name: "Generic1",
+        description: "Generic Product",
+        category: "Technology",
+        brand: "Lenovo",
+        supplier: "Incredible Connection"
+    },
+    {
+        upc: "0002",
+        name: "Generic2",
+        description: "Generic Product",
+        category: "Beauty",
+        brand: "Channel",
+        supplier: "Edgars"
+    },
+    {
+        upc: "0003",
+        name: "Generic3",
+        description: "Generic Product",
+        category: "Software",
+        brand: "Microsoft",
+        supplier: "Evetech"
+    },
+    {
+        upc: "0004",
+        name: "Generic4 But this is a really long name",
+        description: "Generic Product but this is a really long name",
+        category: "Generic Category but this is a really long name",
+        brand: "Generic Brand but this is a really long name",
+        supplier: "Generic Supplier but this is a really long name"
+    }
+]
+
+let users = [
+    {
+        id: "001",
+        username: "GenericUsername",
+        fullName: "GenericFullName",
+        email: "GenericEmail@gmail.com",
+        user_type: "GenericUserType"
+    },
+    {
+        id: "002",
+        username: "GenericUsername",
+        fullName: "GenericFullName",
+        email: "GenericEmail@gmail.com",
+        user_type: "GenericUserType"
+    },
+    {
+        id: "011",
+        username: "GenericUsername",
+        fullName: "GenericFullName",
+        email: "GenericEmail@gmail.com",
+        user_type: "GenericUserType"
+    },
+    {
+        id: "012",
+        username: "GenericUsername",
+        fullName: "GenericFullName",
+        email: "GenericEmail@gmail.com",
+        user_type: "GenericUserType"
+    }
+]
+
+// Initiliser varibles
+let CategoryInit = 0;
+let StockistInit = 0;
+function webLoad() {
+    //Load side content
+    categoryTmp.forEach((cat) => { sideContentUpdate('categories', cat) });
+    StockistTmp.forEach((stock) => { sideContentUpdate('stockist', stock) });
+
+    products.forEach((prod) => { productsUpdate(prod) });
+    users.forEach((user) => { usersUpdate(user) });
 }
 
 $(document).ready(webLoad);
