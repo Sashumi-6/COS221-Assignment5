78d778c606fdb356db75ce3924a5d04957a5f7da
kikoketchup <nsmzoneli@gmail.com>
2025-05-25 19:42:56 +0200

    Added error paragraph under buttons. ValidateForm function done

--- front-end/signup/signup.js
+++ front-end/signup/signup.js
@@ -12,5 +12,30 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function validateForm(){
-    
+    const name = document.getElementById('name-input').value.trim();
+    const username = document.getElementById('username-input').value.trim();
+    const email = document.getElementById('email-input').value.trim();
+    const password = document.getElementById('password-input').value;
+    const type = document.getElementById('user-type').value;
+    const errorEl = document.getElementById('signup-error');
+
+    errorEl.textContent = '';
+
+    if(!name || !username || !email || !password || !type){
+        errorEl.textContent = 'Please fill out all fields.';
+        return false;
+    }
+
+    const emRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/;
+    if (!emRegex.test(email)) {
+        errorEl.textContent = 'Invalid email format.';
+        return false;
+    }
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$/;
+    if (!passwordRegex.test(password)) {
+        errorEl.textContent = 'Password must be 8+ chars with upper/lowercase, digit & symbol.';
+        return false;
+    }
+    return true;
 }
