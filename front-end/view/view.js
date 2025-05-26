75b14142f1c193efe6d468957ebd6d506b5b251f
Daniel Aidan Cohen <168676041+Sashumi-6@users.noreply.github.com>
2025-05-26 21:41:48 +0200

    starting js for view

---
+++ front-end/view/view.js
@@ -0,0 +1,9 @@
+// Assume the product ID is in the URL as a query
+const urlParams = new URLSearchParams(window.location.search);
+const product_id = urlParams.get('productID');
+
+function webload() {
+
+}
+
+$(document).ready(webload);
